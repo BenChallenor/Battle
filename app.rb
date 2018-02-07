d753748b901f5826cd7995266e10a63bfbebf9af
Ben <ben.challenor@btopenworld.com>
2018-02-07 15:52:21 +0000

    Pass test for entering players names

--- app.rb
+++ app.rb
@@ -3,12 +3,13 @@ require "sinatra/base"
 class Battle < Sinatra::Base
 
   get '/' do
-    'Battle!'
     erb(:index)
   end
 
 
   post "/names" do
+    @player_1 = params[:player_1]
+    @player_2 = params[:player_2]
     erb(:play)
   end
 
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -8,8 +8,8 @@ end
 feature "Entering names" do
   scenario "Player 1 enters their name" do
     visit "/"
-    fill_in "Player 1", :with => "Ben"
-    expect(page).to have_field("Player 1", with: "Ben")
+    fill_in "player_1", :with => "Ben"
+    expect(page).to have_field("player_1", with: "Ben")
 
   end
 end
