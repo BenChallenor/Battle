a429a983745181041118510ddeef192a82004945
Ben <ben.challenor@btopenworld.com>
2018-02-08 10:25:23 +0000

    Added player 2 hit points

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -14,3 +14,13 @@ feature "Entering names" do
     expect(page).to have_content("Ben versus Terry")
   end
 end
+
+feature "Hit points" do
+  scenario "Viewing player 2 hit points" do
+    visit "/"
+    fill_in "player_1", with: "Ben"
+    fill_in "player_2", with: "Hannah"
+    click_button 'Submit'
+    expect(page).to have_content("Ben versus Hannah 50 HP")
+  end
+  end
